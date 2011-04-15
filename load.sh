7e60ee469d1be7b2761d70214a8c0f461a14a269
Austin Matzko <austin.matzko@iostudio.com>
2011-04-15 12:18:49 -0500

    highlighted root shell branch

--- load.sh
+++ load.sh
@@ -1,6 +1,6 @@
 source ~/.bash_scripts/git-completion.sh
 
 # colors with git repo info
-PS1='\h:\w\[\033[32m\]$(__git_ps1) \[\033[0m\]$ '
+PS1='\[\033[31m\](\u)\[\033[0m\] $\h:\w\[\033[32m\]$(__git_ps1) \[\033[0m\]# '
 
 export TERM=xterm-256color
