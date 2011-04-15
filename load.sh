88383878f0fe2fbce13d89ae291f755cdd3930e5
Austin Matzko <austin.matzko@iostudio.com>
2011-04-15 13:15:12 -0500

    display red and # for root

--- load.sh
+++ load.sh
@@ -1,6 +1,12 @@
 source ~/.bash_scripts/git-completion.sh
 
 # colors with git repo info
-PS1='\h:\w\[\033[32m\]$(__git_ps1) \[\033[0m\]$ '
+# test for root
+if [ $(id -u) -eq 0 ];
+then
+	PS1='\[\033[31m\](\u)\[\033[0m\] $\h:\w\[\033[32m\]$(__git_ps1) \[\033[0m\]# '
+else
+	PS1='\h:\w\[\033[32m\]$(__git_ps1) \[\033[0m\]$ '
+fi
 
 export TERM=xterm-256color
