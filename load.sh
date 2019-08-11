3ccd82c52194f7f18ca3ddc7e0c5cb0594cd1abc
Austin Matzko <austin@wecounsel.com>
2019-08-11 19:47:09 -0400

    Aliases for elm tools to the elm_tools docker image

--- load.sh
+++ load.sh
@@ -25,3 +25,8 @@ source "/usr/share/rvm/scripts/rvm"
 # export TERM=xterm-256color
 
 set -o vi
+
+alias elm='docker run -it --rm -v "$(pwd):/code" -w "/code" -e "HOME=/tmp" -u $UID:$GID -p 8000:8000 matzko/elm_tools elm'
+alias elm-generate='docker run -it --rm -v "$(pwd):/code" -w "/code" -e "HOME=/tmp" -u $UID:$GID matzko/elm_tools elm-generate'
+alias elm-format='docker run -it --rm -v "$(pwd):/code" -w "/code" -e "HOME=/tmp" -u $UID:$GID matzko/elm_tools elm-format'
+alias elm-analyse='docker run -it --rm -v "$(pwd):/code" -w "/code" -e "HOME=/tmp" -u $UID:$GID matzko/elm_tools elm-analyse'
