cfb3fae1431a0ccff039d62802154cd9e0c5b3d5
Austin Matzko <austin@wecounsel.com>
2019-08-16 20:53:48 -0400

    Better tooling around elm, using docker images

--- load.sh
+++ load.sh
@@ -28,5 +28,8 @@ set -o vi
 
 alias elm='docker run -it --rm -v "$(pwd):/code" -w "/code" -e "HOME=/tmp" -u $UID:$GID -p 8000:8000 matzko/elm_tools elm'
 alias elm-generate='docker run -it --rm -v "$(pwd):/code" -w "/code" -e "HOME=/tmp" -u $UID:$GID matzko/elm_tools elm-generate'
-alias elm-format='docker run -it --rm -v "$(pwd):/code" -w "/code" -e "HOME=/tmp" -u $UID:$GID matzko/elm_tools elm-format'
 alias elm-analyse='docker run -it --rm -v "$(pwd):/code" -w "/code" -e "HOME=/tmp" -u $UID:$GID matzko/elm_tools elm-analyse'
+alias npm='docker run -it --rm -v "$(pwd):/code" -v "/tmp:/tmp" -w "/code" -e "HOME=/tmp" -u $UID:$GID matzko/elm_tools npm'
+alias create-elm-app='docker run -it --rm -v "$(pwd):/code" -w "/code" -e "HOME=/tmp" -u $UID:$GID matzko/elm_tools create-elm-app'
+alias elm-app='docker run -it --rm -v "$(pwd):/code" -w "/code" -e "HOME=/tmp" -u $UID:$GID matzko/elm_tools elm-app'
+PATH=~/.bash_scripts/bin:$PATH
